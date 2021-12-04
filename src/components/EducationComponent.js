df3ce655f3edba3c750311a0faecd4169d5c21d3
JacobMooney <Jacobhmoon@gmail.com>
2021-12-04 14:34:15 -0600

    more content to education component, updated graphic

--- src/components/EducationComponent.js
+++ src/components/EducationComponent.js
@@ -89,35 +89,43 @@ class EducationPage extends Component {
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Reps</td>
+                                <td><strong>Reps</strong></td>
                                 <td>The amount of repetitions preformed per set. | Example; 1 single push up is 1 rep or repetition.</td>
                             </tr>
                             <tr>
-                                <td>Sets</td>
+                                <td><strong>Sets</strong></td>
                                 <td>The amount of times you preform a lift for a set of reps. | Example; 5 push ups in sequence, followed by a break, and then 5 more push ups would be 2 sets of 5 reps.</td>
                             </tr>
                             <tr>
-                                <td>Reps X Sets</td>
+                                <td><strong>Reps X Sets</strong></td>
                                 <td>Commonly how reps and sets are written in routines. | Example; 3 x 10 would mean 10 repetitions for each of the 3 sets.</td>
                             </tr>
                             <tr>
-                                <td>1 Rep Max</td>
-                                <td>The absolute maximum amount of weight you can lift on a given exercise for 1 repetition. Sometimes tested, but often calculated using a 1 rep max calculator for saftey.</td>
+                                <td><strong>1 Rep Max</strong></td>
+                                <td>The absolute maximum amount of weight you can lift on a given exercise for 1 repetition. Sometimes tested, but often calculated using a 1 rep max calculator for safety.</td>
                             </tr>
                             <tr>
-                                <td>Barbell</td>
+                                <td><strong>Barbell</strong></td>
                                 <td>Long bars that can be loaded with individual weights/plates.</td>
                             </tr>
                             <tr>
-                                <td>Dumbbell</td>
+                                <td><strong>Dumbbell</strong></td>
                                 <td>Shorter bars that have fixed weights, often incrementing by 5lbs.</td>
                             </tr>
                             <tr>
-                                <td>Strength</td>
+                                <td><strong>Compound Exercise</strong></td>
+                                <td>Exercises that work multiple muscle groups at once. Bench Press, Deadlift, Squat, Pull Ups, Over Head Press, etc.</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Isolation Exercise</strong></td>
+                                <td>Exercises that mainly target one specific muscle group. Dumbbell Curl, Dumbbell or Machine Chest Fly, etc.</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Strength</strong></td>
                                 <td>Measures and describes how much weight can be lifted.</td>
                             </tr>
                             <tr>
-                                <td>Hypertrophy</td>
+                                <td><strong>Hypertrophy</strong></td>
                                 <td>The effect of an exercises ability to increase muscle size.</td>
                             </tr>
                         </tbody>
@@ -144,7 +152,7 @@ class EducationPage extends Component {
                     <div> <strong>Week 4</strong> - Bench Press : 85lbs for 3 sets X 8 reps</div>
 
                     <h2 class="mt-4">Training for Strength or Hypertrophy</h2>
-                    <p>A common misconception people have after learning the above concepts about strength and hypertrophy is that you can only train one at a time. While this is not entirely true or false, it is worth explaining that often routines will be oriented around a specific training goal. Because focusing on one goal at a time often is the most effecient way to train for each area. It's recommended to start with strength training and then change routines when the gains start to diminish.</p>
+                    <p>A common misconception people have after learning the above concepts about strength and hypertrophy is that you can only train one at a time. While this is not entirely true or false, it is worth explaining that often routines will be oriented around a specific training goal. Because focusing on one goal at a time often is the most efficient way to train for each area. It's recommended to start with strength training and then change routines when the gains start to diminish.</p>
 
                     <ul>
                         <li>
@@ -170,21 +178,20 @@ class EducationPage extends Component {
 
                     {/* Highlight the importance of exercise selections - compounds vs isolation / multi-joint vs single-joint */}
                     <h2 class="mt-4">Exercise Selection</h2>
-                    <p>When deciding on the exercises to include in a routine, it's worth considering which ones will provide the most results. In order to make steady progress building a routine intelligently can make it twice as effective.</p>
-                    <h5 class="mt-4">Barbell Exercises</h5>
-                    <p>Lifts using the barbell should make up the foundation of any good routine. Barbell movements are compound movements, which means they work multiple muslces at once. Additionally most of these exercises can be loaded much heavier than other options, making them great for increasing strength.</p>
-
-                    <h5 class="mt-4">Dumbbell Exercises</h5>
-                    <p>Lifts using the barbell should make up the foundation of any good routine. Barbell movements are compound movements, which means they work multiple muslces at once. Additionally most of these exercises can be loaded much heavier than other options, making them great for increasing strength.</p>
-
-                    <h5 class="mt-4">Machine Exercises</h5>
-                    <p>Lifts using the barbell should make up the foundation of any good routine. Barbell movements are compound movements, which means they work multiple muslces at once. Additionally most of these exercises can be loaded much heavier than other options, making them great for increasing strength.</p>
-
-                    <h5 class="mt-4">Isolation Exercises</h5>
-                    <p>Lifts using the barbell should make up the foundation of any good routine. Barbell movements are compound movements, which means they work multiple muslces at once. Additionally most of these exercises can be loaded much heavier than other options, making them great for increasing strength.</p>
+                    <p>When deciding on the exercises to include in a routine, it's worth considering which ones will provide the most results. In order to make steady progress building a routine intelligently can make it twice as effective. Below is general list of the catagories most equipment will fit into and should make up the bulk of any routine.</p>
 
                     <img style={{height: 390}} alt="hierarchy of exercises" src={hierarchy} />
-                    
+                    <ul>
+                        <h5>Barbell Exercises</h5>
+                        <p>Lifts using the barbell should make up the foundation of any good routine. Barbell movements are primarily compound movements, which means they work multiple muscles at once. Additionally most of these exercises can be loaded much heavier than other options, making them great for increasing strength.</p>
+    
+                        <h5 class="mt-4">Dumbbell Exercises</h5>
+                        <p>Dumbbell exercises are important because they can be used to train muscle groups together or in isolation. Additionally they can be used to train the body iso-laterally, or each side on it's own. This is very useful for strengthening and correcting muscular imbalances. Dumbbells force the body to stabilize and preform the lift without assistance from the other side of the body.</p>
+    
+                        <h5 class="mt-4">Machine Exercises</h5>
+                        <p>Machine exercises often get a bad rep for not being "real lifts" but they often excel in their specific muscle building goal. Machines allow you to train for certain lifts before you have the nessecarry strength to preform them. For example; using machines you can train the movement pattern for pull ups, building your arm and lat strength enough to eventually preform them with bodyweight. Another benifit of machines, is they allow you to push yourself closer to failure more safely without the need of a spotter.</p>
+                    </ul>
+
                     {/* List off core compound movements */}
                     <h4 class="mt-1">Compound Movements</h4>
                     {/* Use cases for both */}
