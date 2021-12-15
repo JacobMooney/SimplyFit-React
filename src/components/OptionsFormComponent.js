22f545c591c8b855ba6f10a5033e3b9651da7325
JacobMooney <Jacobhmoon@gmail.com>
2021-12-15 12:01:13 -0600

    fixed responsive issues on options page

--- src/components/OptionsFormComponent.js
+++ src/components/OptionsFormComponent.js
@@ -11,9 +11,9 @@ class OptionsForm extends Component{
                 <Row>
                     <Link to="/home"><Button className="NavigationBtnLeft btn-lg">Go Back</Button></Link>
                 </Row>
-                <Container fluid>
-                    <Row className="optionsRow">
-                        <Col className="col-s-4 optionsCol d-grid gap-1">
+                <div>
+                    <Row className='flex-md-wrap flex-lg-nowrap'>
+                        <Col md='12' lg='4' className="optionsCol d-grid gap-2">
                             <h3 className="optionsHeader">Days Per Week:</h3>
                             <ButtonGroup vertical>
                                 <Button className="optionsBtn" value={2} name={"daysSelected"} onClick={this.props.handleChange}>2</Button>
@@ -22,7 +22,7 @@ class OptionsForm extends Component{
                             </ButtonGroup>
                             <h5>Workout for {currentInfo.daysSelected} days a week.</h5>
                         </Col>
-                        <Col className="col-s-4 optionsCol d-grid gap-1">
+                        <Col md='12' lg='4' className="optionsCol d-grid gap-1">
                             <h3 className="optionsHeader">Training Goal:</h3>
                             <ButtonGroup vertical>
                                 <Button className="optionsBtn" value={'Strength'} name={"goalSelected"} onClick={this.props.handleChange}>Strength</Button>
@@ -31,7 +31,7 @@ class OptionsForm extends Component{
                             </ButtonGroup>
                             <h5>Selected: {currentInfo.goalSelected}</h5>
                         </Col>
-                        <Col className="col-s-4 optionsCol d-grid gap-1">
+                        <Col md='12' lg='4' className="optionsCol d-grid gap-1">
                             <h3 className="optionsHeader">Additional Options:</h3>
                             <ButtonGroup vertical>
                                 <Button className="optionsBtn" value={currentInfo.cardio} name={"cardio"} onClick={this.props.toggleChange}>Cardio</Button>
@@ -40,7 +40,7 @@ class OptionsForm extends Component{
                             <h5>Selected: {currentInfo.stretching ? "Stretching" : ""} {currentInfo.cardio ? "Cardio" : ""}</h5>
                         </Col>
                     </Row>
-                </Container>
+                </div>
                 <Row className="submitRow">
                     <Col>
                         <Link to="/workoutDisplay"><Button className="NavigationBtnRight btn-lg">View Routine</Button></Link>
